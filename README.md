fb0766f362816216065a66e014bf4ce4e010304e
czh_home <5741034@qq.com>
2018-07-28 11:28:46 +0800

    QQ:5741034

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## JsonAdapter
 
-简单页面adapter封装，直接传入JsonArray。
-减少Bean类文件，如果遇到经常修改字段的后台，用这个就不用到处改了。
-复杂页面，经常修改数据的时候，JsonArray不方便修改，就不要使用这个了。
+* 可以直接传入JsonArray的adapter精简封装，占用体积小
+* 简单页面，不想写bean类的时候，直接传入JsonArray。
+* 减少Bean类文件，如果遇到经常修改字段的后台，用这个就不用到处改了。
+* 复杂页面，经常修改数据的时候，JsonArray不方便修改，就不要使用这个了。
 
 * 仅适用kotlin开发者
 * 仅适用kotlin开发者
@@ -18,10 +19,10 @@ repositories {
   google()
 }
 dependencies {
-  implementation 'com.github.czh235285:JsonAdapter:1.0.1'
+  implementation 'com.github.czh235285:JsonAdapter:1.0.2'
 }
 ```
-## 介绍
+## 用法介绍
 
 使用代码 :
 
@@ -37,20 +38,26 @@ class TestAdapter(mLayoutResId: Int, mData: JSONArray?) : JsonAdapter(mLayoutRes
 
 ```
 
-支持空布局（添加头部、尾部未写，有需要的Issues） :
+支持空布局、添加头部、尾部:
 
 ```
 
    adapter.setEmptyView(layoutInflater.inflate(R.layout.empty,null))
+   adapter.addHeaderView(layoutInflater.inflate(R.layout.head, null))
+   adapter.addFooterView(layoutInflater.inflate(R.layout.foot, null))
 
 ```
 
-item点击事件
+item点击/长按事件:
 
 ```
 
-   adapter.setOnItemClickListener { adapter, view, position ->
-            TODO()
+    adapter.setOnItemClickListener { view, position, item ->
+            Toast.makeText(this@MainActivity, "点击了position==$position,item=${item.optString("name")}", Toast.LENGTH_LONG).show()
+        }
+        
+	adapter.setOnItemLongClickListener { view, position, item ->
+            Toast.makeText(this@MainActivity, "长按了position==$position,item=${item.optString("name")}", Toast.LENGTH_LONG).show()
         }
 
 ```
--- app/src/main/java/com/czh/adapter/MainActivity.kt
+++ app/src/main/java/com/czh/adapter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.czh.adapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
@@ -29,7 +30,29 @@ class MainActivity : AppCompatActivity() {
         val a = JSONObject(s).getJSONArray("a")
         val adapter = TestAdapter(R.layout.item, a)
         adapter.setEmptyView(layoutInflater.inflate(R.layout.empty, null))
+
         rcv.adapter = adapter
 
+        adapter.setHeaderFooterEmpty(true, false)
+
+        adapter.setOnItemClickListener { view, position, item ->
+            Toast.makeText(this@MainActivity, "点击了position==$position,item=${item.optString("name")}", Toast.LENGTH_LONG).show()
+        }
+
+        adapter.setOnItemLongClickListener { view, position, item ->
+            Toast.makeText(this@MainActivity, "长按了position==$position,item=${item.optString("name")}", Toast.LENGTH_LONG).show()
+        }
+        tv1.setOnClickListener {
+            adapter.replaceData(null)
+        }
+        tv2.setOnClickListener {
+            adapter.addHeaderView(layoutInflater.inflate(R.layout.head, null))
+        }
+        tv3.setOnClickListener {
+            adapter.addFooterView(layoutInflater.inflate(R.layout.foot, null))
+        }
+        tv4.setOnClickListener {
+            adapter.addData(JSONObject(s).getJSONArray("a"))
+        }
     }
 }
