142bbbf6af5733089f0cc4ccc45efdea0967cbfd
czh_home <5741034@qq.com>
2018-08-21 00:42:24 +0800

    add anko

--- app/src/main/java/com/czh/adapter/DemoAdapter.kt
+++ app/src/main/java/com/czh/adapter/DemoAdapter.kt
@@ -1,6 +1,8 @@
 package com.czh.adapter
 
+import android.view.Gravity
 import android.view.View
+import android.widget.TextView
 import czh.library.SimpleAnkoAdapter
 import czh.library.BaseViewHolder
 import org.jetbrains.anko.*
@@ -18,4 +20,22 @@ class DemoAdapter(mData: List<String>?) : SimpleAnkoAdapter<String>(mData) {
 
 }
 
+class DemoAdapterUI : AnkoComponent<DemoAdapter> {
+    lateinit var tv: TextView
 
+    override fun createView(ui: AnkoContext<DemoAdapter>) = with(ui) {
+        verticalLayout {
+            tv = textView("测试") {
+                textSize = 20f
+                setShape("#cccc00", 10f)
+                setPadding(20, 20, 20, 20)
+                colorString("#ffffff")
+                gravity = Gravity.CENTER
+            }.lparams(dip(180)) {
+
+                setMargins(20, 20, 20, 20)
+            }
+
+        }
+    }
+}
